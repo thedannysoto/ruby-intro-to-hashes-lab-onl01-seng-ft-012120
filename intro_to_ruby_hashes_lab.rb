07d293918fe2248ba08a09370b96d1a79c64e8f7
Danny Soto <87dsoto@gmail.com>
2020-01-23 21:10:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,9 @@ def monopoly_with_second_tier
 		monopoly = {
 	  railroads: {
 	    pieces: 4,
-	    names: {},
+	    names: {
+	      
+	    },
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
 	      two_pieces_owned: 50,
